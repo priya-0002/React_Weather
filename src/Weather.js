7a114d4ec8f0f324f9230603d62515b8921ff002
priya-002 <priya.md0002@gmail.com>
2021-05-02 19:32:59 +0530

    first commit

--- src/Weather.js
+++ src/Weather.js
@@ -6,7 +6,7 @@ import {useEffect,useState} from 'react'
 
 
 const Weather = () => {
-    const [weather,setWeather]=useState('')
+    const [weather,setWeather]=useState({})
     useEffect(() => {
         fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=f062124b807daaa28c43206c00f668b1')
         .then((res)=>{
@@ -23,6 +23,7 @@ const Weather = () => {
         <div>
             <h1>Weather app</h1>
             <h3>{weather.main.temp}</h3>
+            <h5>{weather.weather[0].description}</h5>
             
         </div>
     )
